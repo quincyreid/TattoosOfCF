46914d20fa05056942638ad5a574a9c156b02163
Quincy Reid <qreid51@gmail.com>
2013-08-02 16:19:54 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TattoosOfCF::Application.config.secret_token = '7a916e9d8cb59ec99673b2909e99b16c6a086c173b70ae0df6f25adea1ac1a635db70ba21ca4415ccde4e0ddf3991d0b65631a36662872c1fc6f53bc53a31327'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TattoosOfCF::Application.config.session_store :cookie_store, key: '_TattoosOfCF_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TattoosOfCF::Application.config.session_store :active_record_store
